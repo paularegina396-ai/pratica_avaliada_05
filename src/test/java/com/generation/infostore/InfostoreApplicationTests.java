31a63c4f8d5ec6b5ca31502e3ad82c6b60e1d941
paula_leao <paularegina396@gmail.com>
2026-07-22 16:12:41 -0300

    :tada: Commit inicial

---
+++ src/test/java/com/generation/infostore/InfostoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.infostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfostoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
